f4f7d2a85c911e3d3d57fe57c8810a1adf27c490
Kasey Keahey <Kasey_Keahey@yahoo.com>
2021-07-27 08:44:32 -0500

    test

--- AE-Scraper.py
+++ AE-Scraper.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-
+#test22
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
